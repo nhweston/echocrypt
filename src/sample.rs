1efaecdd164f73a7e59f67fbf97c8b21c5f116c0
nhweston <nhweston1997@gmail.com>
2020-09-03 23:54:32 +1000

    perf: use 'count_ones' method

--- src/sample.rs
+++ src/sample.rs
@@ -10,24 +10,12 @@ impl Sample for i16 {
 
 impl Sample for u16 {
     fn aggregate_sample(self) -> bool {
-        let mut val = self;
-        let mut res = val;
-        for _ in 1..16 {
-            val <<= 1;
-            res ^= val;
-        }
-        res & 1 != 0
+        self.count_ones() & 1 != 0
     }
 }
 
 impl Sample for f32 {
     fn aggregate_sample(self) -> bool {
-        let mut val = u32::from_le_bytes(self.to_ne_bytes());
-        let mut res = val;
-        for _ in 1..32 {
-            val >>= 1;
-            res ^= val;
-        }
-        res & 1 != 0
+        u32::from_le_bytes(self.to_ne_bytes()).count_ones() & 1 != 0
     }
 }
