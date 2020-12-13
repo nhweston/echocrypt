5a901a852cf742c8f22dff64e549d5e9ba2ac9db
nhweston <nhweston1997@gmail.com>
2020-12-13 21:40:54 +1100

    style: clean up code

--- src/main.rs
+++ src/main.rs
@@ -28,38 +28,38 @@ fn run() -> Result<()> {
     let params = Params::new(env::args().collect())?;
     let host = cpal::default_host();
     let device = host.default_input_device().ok_or(anyhow!("No device available"))?;
-    let (conf, samp_fmt) = {
-        let conf = device.default_input_config()?;
-        (conf.config(), conf.sample_format())
+    let (config, sample_format) = {
+        let config = device.default_input_config()?;
+        (config.config(), config.sample_format())
     };
-    match samp_fmt {
-        I16 => start::<i16>(device, conf, params),
-        U16 => start::<u16>(device, conf, params),
-        F32 => start::<f32>(device, conf, params),
+    match sample_format {
+        I16 => start::<i16>(device, config, params),
+        U16 => start::<u16>(device, config, params),
+        F32 => start::<f32>(device, config, params),
     }
 }
 
 pub fn start<T: Sample>(
-    dev: Device,
-    conf: StreamConfig,
+    device: Device,
+    config: StreamConfig,
     params: Params,
 ) -> Result<()> {
     let (tx, rx) = channel::<Result<()>>();
     let Params { charset, pwd_len, num_pwds } = params;
-    let mut gen = Generator::new(charset, pwd_len);
+    let mut generator = Generator::new(charset, pwd_len);
     let mut mutex = Mutex::new(tx);
     let mut num_pwds_remaining = num_pwds;
     let border = "─".repeat(pwd_len);
     println!("┌{}┐", border);
-    let stream = dev.build_input_stream(
-        &conf.into(),
+    let stream = device.build_input_stream(
+        &config.into(),
         move |data: &[T], _| match mutex.get_mut() {
             Ok(tx) => {
                 let mut vec = Vec::new();
-                for &samp in data {
-                    vec.push(samp.aggregate_sample());
+                for &sample in data {
+                    vec.push(sample.aggregate_sample());
                 }
-                for pwd in gen.push(&vec) {
+                for pwd in generator.push(&vec) {
                     match String::from_utf8(pwd) {
                         Ok(pwd) => println!("│{}│", pwd),
                         Err(_) => println!("(invalid UTF string)"),
